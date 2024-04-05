daf3ea29c2e6e275756bef9afa061f2745ab0a08
jesmithangam <113496257+jesmithangam@users.noreply.github.com>
2024-04-05 17:46:13 +0530

    added index, login, and events page for web

--- TN_APP_Web/www/js/routes.js
+++ TN_APP_Web/www/js/routes.js
@@ -1,16 +1,18 @@
-
-var routes = [
-  {
+var routes = [{
     path: '/',
     url: './index.html',
-  },
-  {
+},
+{
     path: '/login/',
     url: './pages/login.html',
-  },
-  // Default route (404 page). MUST BE THE LAST
-  {
+},
+{
+    path: '/events/',
+    url: './pages/events.html',
+},
+// Default route (404 page). MUST BE THE LAST
+{
     path: '(.*)',
     url: './pages/404.html',
-  },
+},
 ];
