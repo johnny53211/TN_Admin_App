173ce8c03ecb415c817e2deb3cd6e7695dd402d7
jesmithangam <113496257+jesmithangam@users.noreply.github.com>
2024-04-12 18:05:17 +0530

    updated events page UI

--- TN_APP_Web/www/js/app.js
+++ TN_APP_Web/www/js/app.js
@@ -3,6 +3,7 @@ var app = new Framework7({
   name: 'Employee Portal', // App name
   theme: 'auto', // Automatic theme detection
   el: '#app', // App root element
+  animateNavBackIcon:true,
   
   // Cordova Statusbar settings
   routes: routes,
