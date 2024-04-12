88e9b2dac52c9de2504721a50b8adb2c255011d5
jesmithangam <113496257+jesmithangam@users.noreply.github.com>
2024-04-12 19:43:37 +0530

    navbar UI added to event page

--- TN_APP_Web/www/js/app.js
+++ TN_APP_Web/www/js/app.js
@@ -4,21 +4,19 @@ var app = new Framework7({
   theme: 'auto', // Automatic theme detection
   el: '#app', // App root element
   animateNavBackIcon:true,
+  options: {
+    history: false
+  },
+  preloadPreviousPage: false,
   
   // Cordova Statusbar settings
   routes: routes,
-  swipeBackPage: false,
-  cache: false,
-  tapHold: true,
   modalButtonOk: 'Ok',
   modalButtonCancel: 'Cancel'
 });
 
 mainView = app.views.create('.view-main', {
   // Because we use fixed-through navbar we can enable dynamic navbar   
-  dynamicNavbar: true,
-  domCache: false,
   preloadPreviousPage: false,
-  swipeBackPage: false,
   animatePages: true,
 });
