e5c9cc97e24be0b12f8d3f0c679ae9ccadee3c30
johnny53211 <113012359+johnny53211@users.noreply.github.com>
2024-04-22 09:05:05 +0530

    remove debugger in document ready function in web page

--- TN_APP_Web/www/js/index.js
+++ TN_APP_Web/www/js/index.js
@@ -1,8 +1,7 @@
 
 $(document).ready(function () {
     localStorage.removeItem('isShare');
-    let isSharePage = utils.isSharePage();
-    debugger
+    let isSharePage = utils.isSharePage()
     if (isSharePage && Object.keys(isSharePage).length) {
         utils.redirect({ path: '/events/', query: isSharePage });
     }
